0b3b4f37839d1b3dd1ae8c0be9cde50b0a7ec67e
dorian <dorian4k56@gmail.com>
2022-05-05 12:26:18 +0300

    DONE ALL

--- src/test/java/das/project/employeemanager/service/EmployeeServiceTest.java
+++ src/test/java/das/project/employeemanager/service/EmployeeServiceTest.java
@@ -5,32 +5,42 @@ import das.project.employeemanager.model.Employee;
 import das.project.employeemanager.repo.EmployeeRepo;
 import das.project.employeemanager.service.imp.EmployeeServiceImp;
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.reactive.function.BodyInserters;
+
+import static org.assertj.core.api.Assertions.as;
 import static org.mockito.BDDMockito.given;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @ExtendWith(SpringExtension.class)
@@ -41,11 +51,14 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 class EmployeeServiceTest {
 
     @Autowired
-    @InjectMocks
     private EmployeeServiceImp employeeService;
 
     @Autowired
+    @Mock
     private EmployeeRepo employeeRepo;
+
+    @Mock
+    private EmployeeDTO employeeDTO;
 
     private String serverURL;
 
@@ -64,6 +77,7 @@ class EmployeeServiceTest {
     }
 
     @Test
+    @DisplayName("Test addEmployee() - method that add a new employee")
     void addEmployee() {
 
         EmployeeDTO employee = new EmployeeDTO();
@@ -102,41 +116,48 @@ class EmployeeServiceTest {
         assertEquals(employee.getPhone(), savedEmployee.getPhone());
         assertEquals(employee.getJobTitle(), savedEmployee.getJobTitle());
         assertEquals(employee.getImageUrl(), savedEmployee.getImageUrl());
-
     }
 
     @Test
+    @DisplayName("Test gotAllEmployees() - return list of all employees")
     void gotAllEmployees() {
-//        List<EmployeeDTO> allEmployee = employeeRepo.findAll();
-//        assertThat(allEmployee.size()).isGreaterThanOrEqualTo(1);
         EmployeeDTO employee3 = new EmployeeDTO();
-        employee3.setId(3L);
         employee3.setName("Pavel Borodac");
         employee3.setPhone("24-333-222");
         employee3.setEmail("pavel@gmail.com");
         employee3.setJobTitle("Java Junior");
         employee3.setImageUrl("https://www.bootdey.com/app/webroot/img/Content/avatar/avatar1.png");
-        EmployeeDTO employee1 = new EmployeeDTO();
-        employee1.setId(1L);
-        employee1.setName("Pavel Borodac");
-        employee1.setPhone("24-333-222");
-        employee1.setEmail("pavel@gmail.com");
-        employee1.setJobTitle("Java Junior");
-        employee1.setImageUrl("https://www.bootdey.com/app/webroot/img/Content/avatar/avatar1.png");
 
-//        given(employeeService.findAllEmployees()).willReturn(List.of(employee3, employee1));
-//        List<EmployeeDTO> allEmployeeList = employeeService.findAllEmployees();
-//        assertThat(allEmployeeList.size()).isGreaterThanOrEqualTo(1);
-//        assertThat(allEmployeeList).containsExactly(employee3,employee1);
+        EmployeeDTO employee1 = new EmployeeDTO();
+        employee1.setName("Dorian Borodac");
+        employee1.setPhone("24-233-112");
+        employee1.setEmail("dorian@gmail.com");
+        employee1.setJobTitle("Java Juor");
+        employee1.setImageUrl("https://www.bootdey.com/app/webroot/img/Content/avatar/avatar2.png");
+
         List<EmployeeDTO> employeeDTOList = new ArrayList<>();
         employeeDTOList.add(employee3);
         employeeDTOList.add(employee1);
 
+        Assertions.assertEquals(2, employeeDTOList.size(), "findAllEmployees should return 2 employees");
+        Assertions.assertEquals("Dorian Borodac", employee1.getName());
+        Assertions.assertEquals("Pavel Borodac", employee3.getName());
 
+        assertThat(employeeDTOList.get(0).getName()).isEqualTo(employee3.getName());
+        assertThat(employeeDTOList.get(0).getPhone()).isEqualTo(employee3.getPhone());
+        assertThat(employeeDTOList.get(0).getEmail()).isEqualTo(employee3.getEmail());
+        assertThat(employeeDTOList.get(0).getJobTitle()).isEqualTo(employee3.getJobTitle());
+        assertThat(employeeDTOList.get(0).getImageUrl()).isEqualTo(employee3.getImageUrl());
 
+        assertThat(employeeDTOList.get(1).getName()).isEqualTo(employee1.getName());
+        assertThat(employeeDTOList.get(1).getPhone()).isEqualTo(employee1.getPhone());
+        assertThat(employeeDTOList.get(1).getEmail()).isEqualTo(employee1.getEmail());
+        assertThat(employeeDTOList.get(1).getJobTitle()).isEqualTo(employee1.getJobTitle());
+        assertThat(employeeDTOList.get(1).getImageUrl()).isEqualTo(employee1.getImageUrl());
     }
 
     @Test
+    @DisplayName("Test updateEmployee - update a new employee")
     void updateEmployee() {
         EmployeeDTO employee3 = new EmployeeDTO();
         employee3.setId(3L);
@@ -212,6 +233,7 @@ class EmployeeServiceTest {
     }
 
     @Test
+    @DisplayName("Test findEmployeeById() - find a saved Employee by Id")
     void findEmployeeById() {
         EmployeeDTO employee1 = new EmployeeDTO();
         employee1.setId(1L);
@@ -266,6 +288,7 @@ class EmployeeServiceTest {
     }
 
     @Test
+    @DisplayName("Test deleteEmployee() - delete employee by Id")
     void deleteEmployee() {
         EmployeeDTO employee3 = new EmployeeDTO();
         employee3.setId(3L);
